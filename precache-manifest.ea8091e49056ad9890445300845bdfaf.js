1c77af30705624cfe3eba45e9934afda25d5adb4
Chiranjeeb Jena <chiranjib.jena@gmail.com>
2019-05-05 12:26:59 +0530

    Updates

---
+++ precache-manifest.ea8091e49056ad9890445300845bdfaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77af84c951dcfa718c26aaed5758a2f6",
+    "url": "/giphy/index.html"
+  },
+  {
+    "revision": "b026d492d5a85fc850da",
+    "url": "/giphy/static/css/main.64e1492b.chunk.css"
+  },
+  {
+    "revision": "850fee966198ae9e162f",
+    "url": "/giphy/static/js/2.3c4a8565.chunk.js"
+  },
+  {
+    "revision": "b026d492d5a85fc850da",
+    "url": "/giphy/static/js/main.a45c911f.chunk.js"
+  },
+  {
+    "revision": "2be08ba7eaf0823a821e",
+    "url": "/giphy/static/js/runtime~main.f83d3e2c.js"
+  }
+]);
