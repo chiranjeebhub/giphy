d4561994dc260a76ab07dbcf4ab78475307ee69f
Chiranjeeb Jena <chiranjib.jena@gmail.com>
2019-05-05 12:24:46 +0530

    ghpages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    gif: [],
+    query: ""
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://api.giphy.com/v1/gifs/trending?api_key=W6j782MUgb8aXKhD289LHocpawNNNcaH&limit=5`,
+        { crossdomain: true }
+      )
+      .then(res => {
+        this.setState({
+          gif: res.data.data
+        });
+        console.log(res.data.data);
+      });
+  }
+
+  handleChange = e => {
+    this.setState({
+      query: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .get(
+        `http://api.giphy.com/v1/gifs/search?q=${
+          this.state.query
+        }&api_key=W6j782MUgb8aXKhD289LHocpawNNNcaH&limit=5`,
+        { crossdomain: true }
+      )
+      .then(res => {
+        this.setState({
+          gif: res.data.data
+        });
+        console.log(res.data.data);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form
+            className="form-inline"
+            style={{ paddingTop: "10px" }}
+            onSubmit={this.handleSubmit}
+          >
+            <p>Gif Search: </p>
+            <div className="form-group mx-sm-3 mb-2">
+              <label className="sr-only">Password</label>
+              <input
+                type="text"
+                className="form-control"
+                id="query"
+                placeholder="Search Query"
+                value={this.state.query}
+                onChange={this.handleChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary mb-2">
+              Search
+            </button>
+          </form>
+        </header>
+        {this.state.gif.map(g => {
+          return (
+            <div style={{ display: "inline" }}>
+              <img src={g.images.fixed_height.url} alt="gif" />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Demo extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
